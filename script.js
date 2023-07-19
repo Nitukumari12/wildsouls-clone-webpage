af2d73d19db3abce041992692d2432f3c92a62fa
Nitukumari12 <nitiyaraj528@gmail.com>
2023-07-20 01:06:37 +0530

    make a page 5

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-let hover = document.querySelector(".hover21");
+let hov = document.querySelector(".line");
+let mm = document.querySelectorAll(".mm")
+
+mm.forEach((elem)=>{
+    elem.addEventListener("hover",() => {
+      console.log("hlw");
+    })
+})
